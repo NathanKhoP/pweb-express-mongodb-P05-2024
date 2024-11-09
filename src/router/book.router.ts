1a3d37da64551a605b2918e2d42935a51c627eae
KenasGaleno <150374973+KenasGaleno@users.noreply.github.com>
2024-11-09 18:46:59 +0700

    Update book.router.ts

--- src/router/book.router.ts
+++ src/router/book.router.ts
@@ -1,10 +1,11 @@
+// src/router/book.route.ts
 import { Router } from 'express';
 import { getAllBooks, getBookById, addNewBook } from '../controllers/book.controller';
 
 const router = Router();
 
-router.get('/books', getAllBooks);
-router.get('/books/:id', getBookById);
-router.post('/books', addNewBook);
+router.get('/', getAllBooks);      // Untuk mendapatkan semua buku
+router.get('/:id', getBookById);   // Untuk mendapatkan buku berdasarkan ID
+router.post('/', addNewBook);      // Untuk menambahkan buku baru
 
 export default router;
